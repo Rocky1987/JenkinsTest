7936b9ff38fecf64a74da1ace051b38da7cfe01c
DESKTOP-H36UF2N\decie <g0952772916@yahoo.com.tw>
2021-06-26 16:53:39 +0800

    2 commit

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! from gogogo");
+            Console.WriteLine("Hello World! from gogogo test");
         }
     }
 }
